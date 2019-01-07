f733fdda53e7f6e41c0113b9a416522019abcb03
ChonJi <remigiusz.chomicki@gmail.com>
2019-01-07 13:22:17 +0100

    Initial commit

---
+++ ExampleTestSuite.py
@@ -0,0 +1,22 @@
+import unittest
+
+class ExampleTestSuite(unittest.TestCase):
+
+    def test_tc1(self):
+        assert True == True
+
+    def test_tc2(self):
+        assert True == True
+
+    def test_tc3(self):
+        assert True == True
+
+    def test_tc4(self):
+        assert True == True
+
+    def test_tc5(self):
+        assert True == False
+
+
+if __name__ == "__main__":
+    unittest.main()
