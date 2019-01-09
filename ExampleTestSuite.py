0cbdadfce4d61923d21602b8f033d0ebd2bdf140
ChonJi <remigiusz.chomicki@gmail.com>
2019-01-09 16:45:31 +0100

    xml report

--- ExampleTestSuite.py
+++ ExampleTestSuite.py
@@ -1,9 +1,11 @@
 import unittest
+import xmlrunner
+
 
 class ExampleTestSuite(unittest.TestCase):
 
     def test_tc1(self):
-        assert True == True
+        assert True == False
 
     def test_tc2(self):
         assert True == True
@@ -12,11 +14,13 @@ class ExampleTestSuite(unittest.TestCase):
         assert True == True
 
     def test_tc4(self):
-        assert True == True
+        assert True == False
 
     def test_tc5(self):
         assert True == True
 
+    print("I've been trying for all day long!")
 
 if __name__ == "__main__":
-    unittest.main()
+    if __name__ == '__main__':
+        with open('results.xml', 'wb') as output:unittest.main(testRunner=xmlrunner.XMLTestRunner(output=output), failfast=False, buffer=False, catchbreak=False)
