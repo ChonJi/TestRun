a5a7960692afdb8f557b3c502f123461e625277f
ChonJi <remigiusz.chomicki@gmail.com>
2019-01-15 23:25:32 +0100

    Optimize imports

--- ReportProvider.py
+++ ReportProvider.py
@@ -1,26 +1,8 @@
-import requests
+import urllib.request
+
 
 class ReportProvider:
 
     def __init__(self, artifact_id):
-
         url = f"https://gitlab.com/ChonJi1983/TestRun/-/jobs/{artifact_id}/artifacts/raw/results.xml"
-        response = requests.get(url)
-        with open('actual_report.xml', 'wb') as output:
-            output.write(response.content)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        urllib.request.urlretrieve(url, 'actual_report.xml')
